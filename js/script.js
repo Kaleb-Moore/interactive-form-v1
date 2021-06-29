14a00c27641edfddbd214551bc618b1b45c56e59
Kaleb-Moore <k4l3bmoore@gmail.com>
2021-06-29 13:35:31 -0600

    Update script.js
    Added payment method logic so it only shows the correct option based on what is selected from the drop down

--- js/script.js
+++ js/script.js
@@ -10,7 +10,14 @@ const otherField = document.getElementById('other-job-role');
 const activities = document.getElementById('activities');
 const activitiesOptions = document.querySelectorAll('#activities-box label input')
 
-let cost = 0;
+const paymentType = document.getElementById('payment');
+const paymentTypeOptions = document.querySelectorAll('#payment option');
+
+let totalActivitiesCost = 0;
+
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+const creditCard = document.getElementById('credit-card');
 
 //When page load set cursor inside Name field
 document.getElementById('name').focus();
@@ -18,6 +25,9 @@ document.getElementById('name').focus();
 //Set page defaults
 shirtColor.setAttribute('disabled', true);
 otherField.hidden = true;
+paymentTypeOptions[1].selected = true;
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
 
 //If 'other' is selected then the text field will show
 jobRoleDropDown.addEventListener('change', (e) => {
@@ -46,15 +56,36 @@ shirtDesign.addEventListener('change', (e) => {
     }
 });
 
+//When a box is checked it adds the cost to the total at the bottom. It will update if activities are removed.
 activities.addEventListener('change', (e) => {
     const checkedCost = parseInt(e.target.getAttribute('data-cost'));
     let updatedCost = document.getElementById('activities-cost');
 
     if (e.target.checked) {
-        cost += checkedCost;
-        updatedCost.innerHTML = `Total: $${cost}`;
+        totalActivitiesCost += checkedCost;
+        updatedCost.innerHTML = `Total: $${totalActivitiesCost}`;
     } else {
-        cost -= checkedCost;
-        updatedCost.innerHTML = `Total: $${cost}`;
+        totalActivitiesCost -= checkedCost;
+        updatedCost.innerHTML = `Total: $${totalActivitiesCost}`;
+    }
+});
+
+//Toggle payment methods
+paymentType.addEventListener('change', (e) => {
+
+    if (e.target.value === 'credit-card') {
+        paypal.style.display = 'none';
+        bitcoin.style.display = 'none';
+        creditCard.style.display = 'block';
+    }
+    if (e.target.value === 'paypal') {
+        bitcoin.style.display = 'none';
+        paypal.style.display = 'block';
+        creditCard.style.display = 'none';
+    }
+    if (e.target.value === 'bitcoin') {
+        bitcoin.style.display = 'block';
+        paypal.style.display = 'none';
+        creditCard.style.display = 'none';
     }
 });
